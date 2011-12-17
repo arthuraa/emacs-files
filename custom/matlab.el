5677916ddc01d0f05350de1527f12c2f1ed8ce05
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2011-12-17 15:27:24 -0500

    Matlab.

--- custom/matlab.el
+++ custom/matlab.el
@@ -1,7 +1,8 @@
 (load-library "matlab-load")
 
 (defun matlab-mode-add-keys ()
-  (local-set-key (kbd "C-c C-c <C-return>") 'matlab-shell-run-region-or-line))
+  (local-set-key (kbd "C-c C-r") 'matlab-shell-run-region-or-line)
+  (local-set-key (kbd "C-c C-l") 'matlab-shell-run-cell))
 
 (add-hook 'matlab-mode-hook 'matlab-mode-add-keys)
 
