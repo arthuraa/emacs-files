0130420fd7e67ad71a10cd4e7251c2db0ef093e6
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2012-02-01 13:34:12 -0500

    Add ott-mode.

---
+++ site-lisp/ott/ottmode.el
@@ -0,0 +1,34 @@
+;;;###autoload
+(define-generic-mode 'ott-mode
+  '("%")
+  '("metavar" "indexvar" "grammar" "embed" "subrules" "contextrules" "substitutions" "single" "multiple" "freevars" "defns" "defn" "by" "homs" "funs" "fun" "parsing" "begincoqsection" "endcoqsection" "coqvariable")
+  '(
+;    ("\\[\\[.+\\]\\]" . 'font-lock-warning-face)
+;    ("\\[\\[[^\\]]+\\]\\]" . 'font-lock-warning-face)
+    ("{{\\([^{}]+{[^{}]*}\\)*[^{}]*}}" . 'font-lock-doc-face)
+;    ("{{[^}]+}}" . 'font-lock-doc-face)
+    ("(\\+.+\\+)" . 'font-lock-keyword-face)
+    ("<<.*" . 'font-lock-keyword-face)
+    (">>" . 'font-lock-keyword-face)
+    ("</" . 'font-lock-keyword-face)
+    ("//" . 'font-lock-keyword-face)
+    ("IN" . 'font-lock-keyword-face)
+    ("/>" . 'font-lock-keyword-face)
+    (" | " . 'font-lock-keyword-face)
+    (";" . 'font-lock-keyword-face)
+    ("<::" . 'font-lock-keyword-face)
+    ("_::" . 'font-lock-keyword-face)
+    ("::=" . 'font-lock-keyword-face)
+    ("::" . 'font-lock-keyword-face)
+    )
+    (list "\\.ott\\'")
+;  '(".keymap\\'" ".map\\'")
+  nil
+  "Major mode for editing ott format files.")
+
+
+;    ("\\%.*" . 'font-lock-doc-face)
+
+;;;###autoload(add-to-list 'auto-mode-alist '("\\.ott" . ott-mode))
+
+(provide 'ottmode)
