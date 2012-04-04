ed775a47e9d63acb97b005c1a8b63781c3f655a8
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2012-04-04 10:31:36 -0400

    Correct previous commit.

--- custom/proof.el
+++ custom/proof.el
@@ -1,10 +1,7 @@
 (defun set-proof-general-keys ()
   (local-set-key (kbd "C-c C-k") 'proof-goto-point))
 
-(defun set-proof-general-vars ()
-  (setq proof-three-window-enable t))
-
 (add-hook 'coq-mode-hook 'set-proof-general-keys)
-(add-hook 'coq-mode-hook 'set-proof-general-vars)
 
+(setq proof-three-window-enable t)
 (setq proof-splash-enable nil)
