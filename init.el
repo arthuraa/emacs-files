427f081663d5603baa11fca986517e318b27fd4d
Arthur Azevedo de Amorim <arthur@kakapo.(none)>
2013-03-31 10:40:42 -0400

    Add magic typerex line.

--- init.el
+++ init.el
@@ -21,4 +21,6 @@
 
   (server-start)
 
-  (message (format "Startup time: %fs" (- (float-time) start-time))))
+  (message (format "Startup time: %fs" (- (float-time) start-time)))
+
+  (with-temp-buffer (insert (shell-command-to-string "ocp-edit-mode emacs -load-global-config")) (eval-buffer)))
