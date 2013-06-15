fbb17d390bc1a05f9bdc6f21710ca41b65bc1438
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2013-06-15 16:57:05 -0400

    Load lilypond.

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
   (require 'cl)
 
   (labels ((add-path (path) (add-to-list 'load-path path)))
+    (add-path "/usr/share/emacs/site-lisp")
     (add-path (expand-file-name "~/.emacs.d"))
     (mapc #'add-path (file-expand-wildcards "~/.emacs.d/site-lisp/*")))
 
@@ -18,6 +19,11 @@
   (mapc 'load (file-expand-wildcards "~/.emacs.d/custom/*.el"))
 
   (load "~/.emacs.d/site-lisp/proofgeneral/generic/proof-site.el")
+
+  (condition-case ex
+      (load "lilypond-init.el")
+    ('error
+     (message "lilypond is not available")))
 
   (server-start)
 
