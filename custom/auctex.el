408d2b702d7970f130238daa15d458a5bd84cfff
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-01-10 16:53:40 -0500

    Workaround bug that turns off PDF mode.

--- custom/auctex.el
+++ custom/auctex.el
@@ -1,2 +1,9 @@
-(add-hook 'TeX-mode-hook 'TeX-global-PDF-mode)
+;; Try to work around anoying bug that turns off PDF mode when finding an error
+;; http://tex.stackexchange.com/questions/114316/auctex-global-pdf-mode-stop-working-after-typesetting-error-occurs
+
+(defun my-tex-global-pdf-mode ()
+  (setq TeX-PDF-mode t)
+  (setq TeX-show-compilation t))
+
+(add-hook 'TeX-mode-hook 'my-tex-global-pdf-mode)
 (add-hook 'TeX-mode-hook 'turn-on-auto-fill)
