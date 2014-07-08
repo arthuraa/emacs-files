5b67a0cf8ad68b052eb1b00c3065c16da1e3ca4e
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-07-08 12:28:08 +0200

    Load Agda Input by default.

--- custom/agda.el
+++ custom/agda.el
@@ -1,2 +1,10 @@
 (load-file (let ((coding-system-for-read 'utf-8))
                 (shell-command-to-string "agda-mode locate")))
+
+(load-file
+ (let ((coding-system-for-read 'utf-8))
+   (expand-file-name "agda-input.el"
+                     (file-name-directory
+                      (shell-command-to-string "agda-mode locate")))))
+
+(agda-input-setup)
