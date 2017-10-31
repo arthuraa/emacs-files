16ea12ba14fc5a8db14b5ffced5c3bf519ac7bd3
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2017-10-31 11:36:03 -0400

    Fix default font.

--- custom/ui.el
+++ custom/ui.el
@@ -12,24 +12,18 @@
                                `((previous-frame-size . ,current-frame-size)
                                  (fullscreen . fullboth))))))
 
-(setq deja-vu-mono-sans
-      "-unknown-DejaVu Sans Mono-normal-normal-normal-*-13-*-*-*-m-0-iso8859-1")
-
-(setq deja-vu-mono-sans-bold
-      "-unknown-DejaVu Sans Mono-bold-normal-normal-*-13-*-*-*-m-0-iso8859-1")
-(setq inconsolata
-      "-unknown-Inconsolata-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
-
 (global-set-key [f11] 'toggle-fullscreen)
 
+(set-face-attribute 'default nil :font "DejaVu Sans Mono-10")
+
 (mouse-wheel-mode t)
-(set-default-font deja-vu-mono-sans nil t)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
 (fringe-mode 4)
+(when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
 
 (condition-case ex
     (load-theme 'zenburn t)
