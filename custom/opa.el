df0810c3dd06ba5ae6358705ddf5e455500b81a5
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2017-10-31 14:37:28 -0400

    Remove unneeded files.

--- custom/opa.el
+++
@@ -1,8 +0,0 @@
-(autoload 'opa-classic-mode "opa-mode.el" "OPA editing mode." t)
-(add-to-list 'auto-mode-alist '("\\.opa$" . opa-classic-mode))
-
-(defun opa-mode-customization ()
-  (setq indent-line-function 'opa-indent-line
-        indent-region-function 'opa-indent-region))
-
-(add-hook 'opa-mode-hook 'opa-mode-customization)
